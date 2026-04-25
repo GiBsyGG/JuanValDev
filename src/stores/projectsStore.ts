32ae646b87811a9096d2405db20f7b39fafdf40b
GiBsyGG <jcval2801@gmail.com>
2026-04-25 11:22:48 -0500

    [Deploy] Github Pages Config Actions

--- src/stores/projectsStore.ts
+++ src/stores/projectsStore.ts
@@ -3,48 +3,54 @@ export const ProjectsStore = [
     image_url: "/images/project_1.png",
     title: "Game Knock it out",
     skillsUsed: ["C#", "Unity"],
-    description: "Proyecto de videojuego de plataformas 2D creado en Unity. Desarrollado en la Global Game Jam 2023",
+    description:
+      "Proyecto de videojuego de plataformas 2D creado en Unity. Desarrollado en la Global Game Jam 2023",
     role: "Programación, Game Design",
-    project_url: "https://kevin-parra.itch.io/knock-it-out-the-show"
+    project_url: "https://kevin-parra.itch.io/knock-it-out-the-show",
   },
   {
     image_url: "/images/project_2.png",
     title: "Game Dead's Loot",
     skillsUsed: ["C#", "Unity"],
-    description: "Proyecto de videojuego shooter top-down creado en Unity. Creado para explorar la generación procedural de niveles y la creación de un sistema de armas en el juego.",
+    description:
+      "Proyecto de videojuego shooter top-down creado en Unity. Creado para explorar la generación procedural de niveles y la creación de un sistema de armas en el juego.",
     role: "Programación, Game Design",
-    project_url: "https://gibsygg.itch.io/deads-loot"
+    project_url: "https://gibsygg.itch.io/deads-loot",
   },
   {
     image_url: "/images/project_4.png",
     title: "React TCG Game",
     skillsUsed: ["HTML", "CSS", "JS", "React"],
-    description: "Juego de combate de cartas creado en React. Creado para explorar el uso de estados, efectos y lógica de juego, con la creación de un sistema de combate.",
+    description:
+      "Juego de combate de cartas creado en React. Creado para explorar el uso de estados, efectos y lógica de juego, con la creación de un sistema de combate.",
     role: "Programación Frontend, Game Design",
-    project_url: "https://react-tcg.netlify.app/"
+    project_url: "https://react-tcg.netlify.app/",
   },
   {
     image_url: "/images/project_3.png",
     title: "Aventura UNAL",
     skillsUsed: ["C#", "Unity"],
-    description: "Prototipo de RPG universitario para explorar el campus de la Universidad Nacional de Colombia Sede Medellín.",
+    description:
+      "Prototipo de RPG universitario para explorar el campus de la Universidad Nacional de Colombia Sede Medellín.",
     role: "Programación, Game Design",
-    project_url: "https://gibsydev.site/#nav"
+    project_url: "/#nav",
   },
   {
     image_url: "/images/project_5.png",
     title: "Appterio",
     skillsUsed: ["React"],
-    description: "Aplicación Movil de gestión de bioterios, con asignación de tareas a diferentes roles de usuario, y gestión de animales para su cuidado.",
+    description:
+      "Aplicación Movil de gestión de bioterios, con asignación de tareas a diferentes roles de usuario, y gestión de animales para su cuidado.",
     role: "Desarrollo Movil Frontend",
-    project_url: "https://github.com/GiBsyGG/Appterio-Frontend"
+    project_url: "https://github.com/GiBsyGG/Appterio-Frontend",
   },
   {
     image_url: "/images/project_6.jpg",
     title: "Berracos Espantos",
     skillsUsed: ["C#", "Unity"],
-    description: "Survivor colombiano cooperativo para 1 o 2 jugadores, donde los campesinos Don Pedro y Doña Ana defienden su vereda de los espantos del folclor colombiano.",
+    description:
+      "Survivor colombiano cooperativo para 1 o 2 jugadores, donde los campesinos Don Pedro y Doña Ana defienden su vereda de los espantos del folclor colombiano.",
     role: "Programación, Game Design",
-    project_url: "https://kevin-parra.itch.io/berracos-espantos"
+    project_url: "https://kevin-parra.itch.io/berracos-espantos",
   },
-]
+];
